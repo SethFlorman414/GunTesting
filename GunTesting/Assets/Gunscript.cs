447e2527b6eaaef36c9c4b52d3ff7fbb635b26d1
Seth Florman <18.seth.florman@ksd.org>
2018-03-21 13:57:52 -0700

    stuff

---
+++ GunTesting/Assets/Gunscript.cs
@@ -0,0 +1,65 @@
+﻿
+using UnityEngine;
+
+public class Gunscript : MonoBehaviour {
+
+    // Use this for initialization
+
+    public float damage = 10f;
+    public float range = 100f;
+    public float fireRate = 15f;
+
+    public Camera fpsCam;
+
+    public ParticleSystem muzzleFlash;
+
+    private float nextTimeToFire = 0f;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+
+            nextTimeToFire = Time.time + 1f / fireRate;
+
+            Shoot();
+
+        }
+
+
+        
+
+	}
+
+
+    void Shoot()
+    {
+
+       muzzleFlash.Play();
+
+
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+
+            Debug.Log(hit.transform.name);
+           Target target =  hit.transform.GetComponent<Target>();
+            if(target != null)
+            {
+
+                target.TakeDamage(damage);
+
+            }
+
+        }
+
+
+    }
+
+}
---
+++ GunTesting/Assets/Reload.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reload : MonoBehaviour {
+
+    // Use this for initialization
+
+    public Animator anim;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetKey(KeyCode.R))
+        {
+
+            anim.SetBool("Reload", true);
+
+        }
+        else
+        {
+
+            anim.SetBool("Reload", false);
+
+        }
+		
+	}
+}
