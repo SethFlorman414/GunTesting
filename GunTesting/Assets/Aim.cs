b146f52375b9d3affd39f9727c68a56e390a818e
Seth Florman <18.seth.florman@ksd.org>
2018-03-23 13:54:38 -0700

    Improvements

---
+++ GunTesting/Assets/Aim.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aim : MonoBehaviour {
+
+    // Use this for initialization\
+
+    public Vector3 aimDownSight;
+    public Vector3 hipFire;
+
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		
+	}
+}
