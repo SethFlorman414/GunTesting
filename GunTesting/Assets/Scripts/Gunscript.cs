dd4d1800a4bf358986c5017e3c72c84208acd8fc
Seth Florman <18.seth.florman@ksd.org>
2018-03-29 13:38:45 -0700

    Improvements made to animations

---
+++ GunTesting/Assets/Scripts/Gunscript.cs
@@ -0,0 +1,160 @@
+﻿
+using UnityEngine;
+
+public class Gunscript : MonoBehaviour
+{
+
+    // Use this for initialization
+
+    public float damage = 10f;
+    public float range = 100f;
+    public float fireRate = 15f;
+
+    public int bulletsPerMag;
+    public int bulletsLeft;
+    public int magazine;
+
+    public Animator reload;
+    public Animator aimDownSight;
+
+    public Camera fpsCam;
+
+    public ParticleSystem muzzleFlash;
+
+    private float nextTimeToFire = 0f;
+
+    void Start()
+    {
+
+        PlayerPrefs.SetInt("Magazine", magazine);
+
+        PlayerPrefs.SetInt("BulletCount", bulletsLeft);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AimDownSight();
+        Reload();
+
+        bulletsLeft = PlayerPrefs.GetInt("BulletCount");
+        magazine = PlayerPrefs.GetInt("Magazine");
+
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire && magazine >= 0)
+        {
+            
+            nextTimeToFire = Time.time + 1f / fireRate;
+           
+
+            Shoot();
+
+        }
+
+        
+
+        if (magazine <= 0)
+        {
+
+            reload.SetBool("Reload", true);
+            
+
+        }
+
+    }
+
+
+    void Shoot()
+    {
+
+        muzzleFlash.Play();
+
+
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+
+            magazine--;
+            PlayerPrefs.SetInt("Magazine", PlayerPrefs.GetInt("Magazine") - 1);
+
+            Debug.Log(hit.transform.name);
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+
+                target.TakeDamage(damage);
+
+            }
+
+             
+
+        }
+        
+
+
+    }
+
+
+    void Reload()
+    {
+
+
+        if (bulletsLeft > 0)
+        {
+            int bulletsToLoad = bulletsPerMag - magazine;
+
+            if (bulletsLeft < bulletsToLoad)
+            {
+                bulletsToLoad = bulletsLeft;
+            }
+
+            bulletsLeft -= bulletsToLoad;
+            magazine += bulletsToLoad;
+
+            PlayerPrefs.SetInt("Magazine", magazine);
+            PlayerPrefs.SetInt("bulletsLeft", bulletsLeft);
+
+        }
+
+        if (Input.GetKey(KeyCode.R))
+        {
+
+            reload.SetBool("Reload", true);
+
+        }
+        else
+        {
+
+            reload.SetBool("Reload", false);
+
+        }
+
+
+
+
+        
+
+    }
+
+
+
+    public void AimDownSight()
+    {
+
+        if(Input.GetMouseButton(1))
+        {
+            
+
+            aimDownSight.SetBool("Aiming", true);
+
+        }
+        else
+        {
+
+            aimDownSight.SetBool("Aiming", false);
+
+        }
+    }
+
+    
+}
---
+++ GunTesting/Assets/Scripts/Shoot.cs
@@ -0,0 +1,144 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	
+	public float range = 100f;
+
+    public Animator reload;
+    public Animator aimDownSight;
+	
+
+	public int bulletsLeft = 200; //Total bullets we have
+	public float bulletSpeed = 10f;
+	public int bulletsPerMag = 6; //Bullets per each magazine
+	public int currentBullets; //current bullets in our magazine
+	public int shootDelay = 3;
+
+	public float fireTimer; //Time counter for the delay
+
+    public ParticleSystem muzzleFlash;
+
+
+	// Use this for initialization
+	void Start () {
+		PlayerPrefs.SetInt ("currentBullets", currentBullets);
+		PlayerPrefs.SetInt ("bulletsLeft", bulletsLeft);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+       
+        AimDownSight();
+		
+		currentBullets = PlayerPrefs.GetInt ("currentBullets");
+		bulletsLeft = PlayerPrefs.GetInt ("bulletsLeft");
+
+		if (Input.GetButton ("Fire1")) {
+			if (currentBullets > 0)
+				Fire (); //Execute the fire function if wem press/hold the left mouse button
+			
+		}
+
+		if (Input.GetKey(KeyCode.R)) {
+			if(currentBullets < bulletsPerMag && bulletsLeft > 0)
+                reload.SetBool("Reload", true);
+                Reload ();
+
+        }
+        else
+        {
+
+            reload.SetBool("Reload", false);
+
+        }
+
+		if (fireTimer < bulletSpeed) {
+			fireTimer += Time.deltaTime; //Add into time counter
+		}
+	}
+
+
+
+    //Picking up bullets for the gun
+	void OnCollisionEnter (Collision collision){
+		if(collision.gameObject.tag == "BulletCase"){
+			PlayerPrefs.SetInt ("Bullets", PlayerPrefs.GetInt("Bullets") + 10);
+			Destroy (collision.gameObject);
+		}
+	}
+
+	private void Fire(){
+
+        muzzleFlash.Play();
+
+		if (fireTimer > shootDelay) {
+			
+			Vector3 shootDirection = Camera.main.transform.forward;
+
+			shootDirection.Normalize ();
+			Camera gunCam = Camera.main;//.allCameras[1];
+			Debug.Log (gunCam.name);
+			Vector3 offset = gunCam.transform.forward;
+			offset += 0.2f * gunCam.transform.forward;
+			offset += 0.2f * gunCam.transform.right;
+			offset -= 0.1f * gunCam.transform.up;
+			Vector3 spawnPosition = gunCam.transform.position + offset;
+			//spawnPosition.x += shootDirection.x * 0.2f;
+			
+			
+
+
+			currentBullets--;
+			fireTimer = 0.0f;
+			PlayerPrefs.SetInt ("currentBullets", PlayerPrefs.GetInt ("currentBullets") - 1);
+
+		}
+
+	}
+
+			public void Reload(){
+				
+		if (bulletsLeft > 0) 
+		{
+			int bulletsToLoad = bulletsPerMag - currentBullets;
+
+			if (bulletsLeft < bulletsToLoad) 
+			{
+				bulletsToLoad = bulletsLeft;
+			}
+
+			bulletsLeft -= bulletsToLoad;
+			currentBullets += bulletsToLoad;
+
+			PlayerPrefs.SetInt ("currentBullets", currentBullets);
+			PlayerPrefs.SetInt ("bulletsLeft", bulletsLeft);
+
+		}
+	}
+
+    public void AimDownSight()
+    {
+
+        if (Input.GetMouseButton(1))
+        {
+
+            aimDownSight.SetBool("Aiming", true);
+
+        }
+        else
+        {
+
+            aimDownSight.SetBool("Aiming", false);
+
+        }
+
+
+    }
+
+
+		
+		
+}
